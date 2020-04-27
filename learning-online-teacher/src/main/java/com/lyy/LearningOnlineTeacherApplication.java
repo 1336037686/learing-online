43b333e1269b6d7953bc89312dbcc497cccd8035
LGX <1336037686@qq.com>
2020-04-27 15:52:51 +0800

    实现课程管理

--- learning-online-teacher/src/main/java/com/lyy/LearningOnlineTeacherApplication.java
+++ learning-online-teacher/src/main/java/com/lyy/LearningOnlineTeacherApplication.java
@@ -3,6 +3,9 @@ package com.lyy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * YY IS P
+ */
 @SpringBootApplication
 public class LearningOnlineTeacherApplication {
 
