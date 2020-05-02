c1d2886bb58c5b76d2a220aefbacd7b33c98b692
LGX <1336037686@qq.com>
2020-05-02 17:05:02 +0800

    创建学生工程

---
+++ learning-online-student/src/main/java/com/lyy/LearningOnlineStudentApplication.java
@@ -0,0 +1,13 @@
+package com.lyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningOnlineStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningOnlineStudentApplication.class, args);
+    }
+
+}
